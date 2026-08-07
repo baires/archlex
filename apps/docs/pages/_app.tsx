8db6c3b20539f374eb0b866a5f1bc81e1ff19706
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2026-08-07 12:33:00 -0300

    fix: docs themes and build script

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "@archlex/design";
+import "../styles/nextra-overrides.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
